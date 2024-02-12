13dd01e24ae07f29fee44cacb810c4f8f1f7abca
ABDURAKHMONOV YOKUBJON <159585396+Yoqub1988@users.noreply.github.com>
2024-02-12 15:41:38 +0900

    Add files via upload

---
+++ the third homework.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 15:30:33 2024
+
+@author: yokubjon
+"""
+#salom_alik="hello world"
+#print(salom_alik)
+#habar="Osmonlarga qochib ketaman" 
+#print(habar)
+#habar="Yerga kirib ketaman"
+#print(habar)
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
