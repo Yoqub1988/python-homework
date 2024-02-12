b0d2387171d29c750f5bf8161880eadecb889085
ABDURAKHMONOV YOKUBJON <159585396+Yoqub1988@users.noreply.github.com>
2024-02-12 10:59:32 +0900

    Add files via upload
    this is my first homework

---
+++ homework.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Mon Feb 12 10:51:48 2024
+
+@author: yokubjon
+"""
+
+print("Assalomu alaykum azizlar")
+print("hayirli tong janoblar")
+print(2+4*2)
+print(19/5)
+print(2**4)
