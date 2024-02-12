55e7903af8d4641be5d8cbac5a3add1482068ccb
ABDURAKHMONOV YOKUBJON <159585396+Yoqub1988@users.noreply.github.com>
2024-02-12 15:09:37 +0900

    Add files via upload

---
+++ the second homework.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 14:41:52 2024
+
+@author: yokubjon
+"""
+
+#print(' "nexia", "tico", \'damas\' ko\'rganlar qilar havas')
+#print("22ni 4ga bo\'lganda qancha qoldiq qoladi?")
+#print(22/4)
